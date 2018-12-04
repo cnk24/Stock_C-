df079e7d262dbea7a3d59f6db4f8386eaf7c88d2
ss3589 <41930495+ss3589@users.noreply.github.com>
2018-12-04 13:02:43 +0900

    add

---
+++ KiwoomStock/KiwoomStock/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace KiwoomStock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKiwoom = new AxKHOpenAPILib.AxKHOpenAPI();
+            ((System.ComponentModel.ISupportInitialize)(this.axKiwoom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKiwoom
+            // 
+            this.axKiwoom.Enabled = true;
+            this.axKiwoom.Location = new System.Drawing.Point(722, 3);
+            this.axKiwoom.Name = "axKiwoom";
+            this.axKiwoom.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKiwoom.OcxState")));
+            this.axKiwoom.Size = new System.Drawing.Size(60, 21);
+            this.axKiwoom.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.axKiwoom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKiwoom)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKiwoom;
+    }
+}
+
